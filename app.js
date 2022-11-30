75b6a0f90793ad04ad97550825ba2c16b0465311
milindbasavaraja <milindbasavaraja96@gmail.com>
2022-11-30 13:28:30 -0500

    Adding Pug template to view data dynamically

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const path = require("path");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.routes);
