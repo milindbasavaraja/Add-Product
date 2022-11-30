648bba8c6626e9e2a02088298a551a0d48355ddc
milindbasavaraja <milindbasavaraja96@gmail.com>
2022-11-30 14:42:17 -0500

    Pug Templates

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,11 @@ const rootDir = require("../util/path");
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-products", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
+  //res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res) => {
--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get("/", (req, res, next) => {
   console.log(adminData.products);
   const products = adminData.products;
   //res.sendFile(path.join(rootDir, "views", "shop.html"));
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/shops" });
 });
 
 module.exports = router;
